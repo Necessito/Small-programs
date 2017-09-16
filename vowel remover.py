f58b4b4a5409e68f5c6135cd656b72ad57c9cc8d
Deadly_Cabbage <31901047+JasonPapas@users.noreply.github.com>
2017-09-16 12:35:27 +0300

    Update vowel remover.py

--- vowel remover.py
+++ vowel remover.py
@@ -5,12 +5,11 @@
 input_string = input("Insert a word to remove the vowels from: ")
 print("You inserted the word:",input_string)
 vowels = ["a", "e", "i", "u", "y", "o", "A", "E", "I", "U", "Y", "O"]
-new_string = []
-vowels_removed = []
+new_string = ''
+vowels_removed = ''
 for letter in input_string:
     if letter not in vowels:
-        new_string.append(letter)
+        new_string+=letter
     else:
-        vowels_removed.append(letter)
-newer = ''.join(new_string)
-print("The new word is: ",newer, "\nThe vowels removed are: ",vowels_removed)
+        vowels_removed+=letter+' '
+print("The new word is: ",new_string, "\nThe vowels removed are: ",vowels_removed)
